948f821c67d410b412d100477268e64d781ea24f
kelecss <kerim.lengwiler@hotmail.com>
2025-05-02 10:55:19 +0200

    #Computed Percentage of Female Graduate from STEM #Stored Data for Tableau in "stem_final.csv

---
+++ data_cleaning.R
@@ -0,0 +1,44 @@
+library(readr)
+library(dplyr)
+library(tidyr)
+
+tag <- function(file, field, sex) {
+  read_csv(file) %>%
+    select(geoUnit, year, value) %>%
+    rename(country = geoUnit) %>%
+    mutate(field_of_study = field,
+           sex = sex)
+}
+
+df_nsms_f <- tag("data_NSMS_f.csv", "NSMS", "Female")
+df_ict_f  <- tag("data_ICT_f.csv",  "ICT",  "Female")
+df_emc_f  <- tag("data_EMC_f.csv",  "ECM",  "Female")
+
+df_nsms_m <- tag("data_NSMS_m.csv", "NSMS", "Male")
+df_ict_m  <- tag("data_ICT_m.csv",  "ICT",  "Male")
+df_emc_m  <- tag("data_EMC_m.csv",  "ECM",  "Male")
+
+stem_sum <- bind_rows(
+  df_nsms_f, df_ict_f, df_emc_f,
+  df_nsms_m, df_ict_m, df_emc_m
+)
+
+stem_per_field <- stem_sum %>%
+  pivot_wider(names_from = sex, values_from = value) %>%
+  mutate(total = Female + Male,
+         pct_female_per_field = (Female / total)*100)
+
+stem_total <- stem_per_field %>%
+  group_by(country, year) %>%
+  summarise(
+    female_total = sum(Female, na.rm = TRUE),
+    male_total   = sum(Male, na.rm = TRUE),
+    total        = female_total + male_total,
+    pct_female_stem = (female_total / total)*100,
+    .groups = "drop"
+  )
+
+stem_final <- stem_total %>%
+  select(country, year, pct_female_stem) %>%
+  write_csv("pct_female_stem_over_time.csv")
+
